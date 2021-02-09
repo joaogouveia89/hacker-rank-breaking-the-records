2ad017d167ceea06b0ef4f5baa4d3cc08473fe7a
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-02-09 18:12:48 +0000

    ruby solution ok

--- solutions/ruby/breaking_the_records.rb
+++ solutions/ruby/breaking_the_records.rb
@@ -1,3 +1,20 @@
 def breaking_the_records scores
-	return [2, 4]
+	response = [0, 0]
+	lowest_score = scores[0]
+	highest_score = lowest_score
+
+	scores.each.with_index do |score, idx|
+		unless idx === 0
+			if score > highest_score
+				highest_score = score
+				response[0] = response[0] + 1
+			end
+
+			if score < lowest_score
+				lowest_score = score
+				response[1] = response[1] + 1
+			end
+		end 
+	end
+	return response
 end
